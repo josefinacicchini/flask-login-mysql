c266abd8b02442cfb4dd4121ed8b6b01bb32cd8b
josefinacicchini <96507383+josefinacicchini@users.noreply.github.com>
2022-07-11 16:58:32 -0300

    erase password hash generator

--- src/models/entities/User.py
+++ src/models/entities/User.py
@@ -1,4 +1,4 @@
-from werkzeug.security import check_password_hash, generate_password_hash
+from werkzeug.security import check_password_hash
 
 class User():
     def __init__(self, id, username, password, fullname="") -> None:
@@ -11,4 +11,3 @@ class User():
     def check_password(self, hashed_password, password):
         return check_password_hash(hashed_password, password)
 
-print(generate_password_hash("suscribete"))
