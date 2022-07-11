6ba26fc68f9ebf8f04932ffe52000fb1dd34c6cf
josefinacicchini <96507383+josefinacicchini@users.noreply.github.com>
2022-07-11 16:19:40 -0300

    Handle login request

--- src/app.py
+++ src/app.py
@@ -1,11 +1,21 @@
+from crypt import methods
 from curses import flash
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from config import config
 
 app = Flask(__name__, template_folder='template')
 
 @app.route('/')
 def index():
+    return redirect(url_for('login'))
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method =='POST':
+        print(request.form['username'])
+        print(request.form['password'])
+        return render_template('auth/login.html')
+    else:
     return render_template('auth/login.html')
 
 
