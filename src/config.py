511a534bb9ecf287b7af7878dd3bf7be5a9ae8f1
josefinacicchini <96507383+josefinacicchini@users.noreply.github.com>
2022-07-11 16:30:05 -0300

    Connection with database

--- src/config.py
+++ src/config.py
@@ -1,5 +1,13 @@
-class DevelopmentConfig():
+class Config():
+    SECRET_KEY = 'FJedj@jwd!'
+
+
+class DevelopmentConfig(Config):
     DEBUG = True
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = 'root'
+    MYSQL_DB = 'madretierra/users'
 
 config = {
     'development': DevelopmentConfig
