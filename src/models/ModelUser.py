435749e39d94e5db0042f912c6da9b34dbf096b6
josefinacicchini <96507383+josefinacicchini@users.noreply.github.com>
2022-07-11 20:23:52 -0300

    User login check

---
+++ src/models/ModelUser.py
@@ -0,0 +1,18 @@
+from .entities.User import User
+
+class ModelUser():
+    @classmethod
+    def login(self,db,user):
+        try:
+            cursor = db.connection.cursor()
+            sql = """SELECT id, username, password, fullname FROM users WHERE username = '{}'""".format(user.username)
+            cursor.execute(sql)
+            row = cursor.fetchone()
+            if row != None:
+                user = User(row[0],row[1], User.check_password(row[2], user.password),row[3])
+                return user
+            else:
+                return None
+
+        except Exception as ex:
+            raise Exception(ex)
